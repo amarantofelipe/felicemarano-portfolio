84c5711e5d558b6533ab5196e2cd5e7dd2df28ca
Felice Marano <112284065+amarantofelipe@users.noreply.github.com>
2023-05-24 23:48:38 +0200

    Analytics

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Project } from './components/Project';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import { Contact } from './components/Contact';
 import { Footer } from './components/Footer';
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
       <Project />
       <Contact />
       <Footer />
+      <Analytics />
     </div>
   );
 }
